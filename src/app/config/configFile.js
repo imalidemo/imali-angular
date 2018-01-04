6983bcd5be4efff949b8ecbaed274520c5e791c4
BS-134\BS134 <tarekmahmud@brainstation-23.com>
2018-01-04 19:18:02 +0600

    test commit

--- src/app/config/configFile.js
+++ src/app/config/configFile.js
@@ -1,2 +1,2 @@
 angular.module("BlurAdmin.config", [])
-.constant("environmentConfig", {"API":"https://rehive.com/api/3","COMPANY":"imali_demo","EXCHANGE_API":"http://45.55.183.106:8000/api","ICO_API":"http://localhost:8001/api","XBT_API":"http://localhost:8002/api/1","ETH_API":"http://localhost:8003/api/1"});
+.constant("environmentConfig", {"API":"https://rehive.com/api/3","COMPANY":"imali_demo","EXCHANGE_API":"http://localhost:8000/api","ICO_API":"https://ico.services.rehive.io/api","XBT_API":"https://bitcoin.services.rehive.com/api/1","ETH_API":"https://ethereum.services.rehive.com/api/1"});

a9b0db976673e56c17615675606001544c7c4281
BS-134\BS134 <tarekmahmud@brainstation-23.com>
2018-01-05 12:40:51 +0600

    config dile merge

--- src/app/config/configFile.js
+++ src/app/config/configFile.js
@@ -1,2 +1,2 @@
 angular.module("BlurAdmin.config", [])
-.constant("environmentConfig", {"API":"https://rehive.com/api/3","COMPANY":"test_company_1","ICO_API":"https://ico.services.rehive.io/api","XBT_API":"https://bitcoin.services.rehive.com/api/1","ETH_API":"https://ethereum.services.rehive.com/api/1"});
+.constant("environmentConfig", {"API":"https://rehive.com/api/3","COMPANY":"imali_demo","EXCHANGE_API":"http://45.55.183.106:8000/api","ICO_API":"https://ico.s.services.rehive.io/api","XBT_API":"https://bitcoin.s.services.rehive.com/api/1","ETH_API":"https://ethereum.s.services.rehive.io/api/1"});

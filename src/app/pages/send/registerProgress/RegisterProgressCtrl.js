3327896af5012b388d700e00e04c08495e05f4b9
tsmrafee <tsmrafee@gmail.com>
2018-04-18 20:01:38 +0600

    kyc flow redirection bug fixes

--- src/app/pages/send/registerProgress/RegisterProgressCtrl.js
+++ src/app/pages/send/registerProgress/RegisterProgressCtrl.js
@@ -5,7 +5,7 @@
         .controller('RegisterProgressCtrl', RegisterProgressCtrl);
 
     /** @ngInject */
-    function RegisterProgressCtrl($rootScope,$scope,$http,cookieManagement,environmentConfig,$location,errorToasts,userVerification) {
+    function RegisterProgressCtrl($rootScope, $scope, $http, cookieManagement, environmentConfig, $location, $window, errorToasts, userVerification) {
 
         var vm = this;
         vm.token = cookieManagement.getCookie('TOKEN');
@@ -15,13 +15,13 @@
         $rootScope.mobileVerified = false;
         $rootScope.addressVerified = "n";
         $rootScope.idDocumentsVerified = 'n';
-
+        var alreadyRouteChanged = false;
         $scope.goToGetVerified = function (path) {
             $location.path(path);
         };
 
-        vm.getUserInfo = function(){
-            if(vm.token){
+        vm.getUserInfo = function () {
+            if (vm.token) {
                 $scope.loadingRegisterProgressView = true;
                 $http.get(environmentConfig.API + '/user/', {
                     headers: {
@@ -32,21 +32,18 @@
                     if (res.status === 200) {
                         $scope.user = res.data.data;
                         console.log($scope.user)
-                        if($scope.user.kyc.status=='verified'){
-                            $rootScope.allVerified=true
+                        if ($scope.user.kyc.status == 'verified') {
+                            $rootScope.allVerified = true
                             $location.path('/home');
                         }
                         else {
                             vm.checkingEmailVerfication(res.data.data.email);
-                            if($scope.user.status =='verified') {
+                            vm.checkingMobileVerification($scope.user.mobile_number);
+                            if ($scope.user.status == 'verified') {
                                 $rootScope.addressVerified = "v";
                             }
-                            else if($scope.user.status =='pending') {
+                            else if ($scope.user.status == 'pending') {
                                 $rootScope.addressVerified = "p";
-                                return;
-                            }
-                            else {
-                                return;
                             }
 
                             $http.get(environmentConfig.API + '/user/address/', {
@@ -57,25 +54,32 @@
                             }).then(function (res) {
                                 if (res.status === 200) {
                                     $scope.address = res.data.data;
-                                    if(!$scope.address.line_1 || !$scope.address.city || !$scope.address.country || !$scope.address.postal_code){
-                                        $scope.goToGetVerified("/identity/verification");
-                                    }
-                                    if($scope.address.status =='verified') {
+                                    
+                                    if ($scope.address.status == 'verified') {
                                         $rootScope.addressVerified = "v";
-                                        if($rootScope.emailVerified == true && $rootScope.mobileVerified == true && $rootScope.addressVerified=="v" && $rootScope.idDocumentsVerified=='v'){
+                                        if ($rootScope.emailVerified == true && $rootScope.mobileVerified == true && $rootScope.addressVerified == "v" && $rootScope.idDocumentsVerified == 'v') {
                                             $scope.allVerified = true;
                                         }
                                     }
-                                    else if($scope.address.status =='pending') {
+                                    else if ($scope.address.status == 'pending') {
                                         $rootScope.addressVerified = "p";
+                                        if (!$scope.address.line_1 || !$scope.address.city || !$scope.address.country || !$scope.address.postal_code) {
+                                            if (!alreadyRouteChanged) {
+                                                $scope.goToGetVerified("/identity/verification");
+                                                alreadyRouteChanged = true;
+                                            }
+                                        }
                                     }
                                 }
+
+
+                                vm.getUserDocuments();
                             }).catch(function (error) {
                                 $scope.loadingRegisterProgressView = false;
                                 errorToasts.evaluateErrors(error.data);
                             });
                         }
-                        
+
                     }
 
                 }).catch(function (error) {
@@ -88,42 +92,43 @@
 
         vm.checkingEmailVerfication = function (email) {
             $scope.loadingRegisterProgressView = true;
-            userVerification.verifyEmail(function(err,verified){
-                if(verified){
+            userVerification.verifyEmail(function (err, verified) {
+                if (verified) {
                     $rootScope.emailVerified = true;
-                    if($scope.user.mobile_number==null || $scope.user.mobile_number.length==0){
+                    if ($scope.user.mobile_number == null || $scope.user.mobile_number.length == 0) {
                         $scope.goToGetVerified("/mobile/verify");
+                        alreadyRouteChanged = true;
                     }
-                    vm.checkingMobileVerification($scope.user.mobile_number);
                 } else {
                     $scope.goToGetVerified("/email/verify");
+                    alreadyRouteChanged = true;
                     $rootScope.emailVerified = false;
                     $scope.loadingRegisterProgressView = false;
                 }
-            },email);
+            }, email);
         };
 
         vm.checkingMobileVerification = function (number) {
             $scope.loadingRegisterProgressView = true;
-            userVerification.verifyMobile(function(err,verified){
-                if(verified){
+            userVerification.verifyMobile(function (err, verified) {
+                if (verified) {
                     $rootScope.mobileVerified = true;
-                    if($rootScope.emailVerified == true && $rootScope.mobileVerified == true && $rootScope.addressVerified=="v" && $rootScope.idDocumentsVerified=="v"){
+                    if ($rootScope.emailVerified == true && $rootScope.mobileVerified == true && $rootScope.addressVerified == "v" && $rootScope.idDocumentsVerified == "v") {
                         $scope.allVerified = true;
                     }
                     $scope.loadingRegisterProgressView = false;
                 } else {
                     $rootScope.mobileVerified = false;
                     $scope.goToGetVerified("/mobile/verify");
+                    alreadyRouteChanged = true;
                     $scope.loadingRegisterProgressView = false;
                 }
-                
-            vm.getUserDocuments();
-            },number);
+
+            }, number);
         };
 
-        vm.getUserDocuments = function(){
-            if(vm.token) {
+        vm.getUserDocuments = function () {
+            if (vm.token) {
                 $http.get(environmentConfig.API + '/user/documents/', {
                     headers: {
                         'Content-Type': 'application/json',
@@ -134,11 +139,15 @@
                         $scope.idDocuments = res.data.data.results.filter(function (element) {
                             return element.document_category == 'Proof Of Identity';
                         });
-                        if($scope.idDocuments.length==0){
-                            $scope.goToGetVerified("/document/verify/ID")
+                        if ($scope.idDocuments.length == 0) {
+                            if (!alreadyRouteChanged) {
+                                $scope.goToGetVerified("/document/verify/ID")
+                                alreadyRouteChanged = true;
+                            }
+
                         }
                         $rootScope.idDocumentsVerified = vm.checkDocumentsArrayVerification($scope.idDocuments);
-                        if($rootScope.emailVerified == true && $rootScope.mobileVerified == true && $rootScope.addressVerified==true && $rootScope.idDocumentsVerified=='v'){
+                        if ($rootScope.emailVerified == true && $rootScope.mobileVerified == true && $rootScope.addressVerified == true && $rootScope.idDocumentsVerified == 'v') {
                             $scope.allVerified = true;
                         }
                     }
@@ -149,29 +158,29 @@
             }
         };
 
-        vm.checkDocumentsArrayVerification = function(documentsArray){
-            if(documentsArray.length === 0){
+        vm.checkDocumentsArrayVerification = function (documentsArray) {
+            if (documentsArray.length === 0) {
                 return 'n';
             } else {
-                for(var i = 0; i < documentsArray.length; i++){
-                    if(documentsArray[i].status === 'verified'){
+                for (var i = 0; i < documentsArray.length; i++) {
+                    if (documentsArray[i].status === 'verified') {
                         return 'v';
                     }
                 }
-                for(var i = 0; i < documentsArray.length; i++){
-                    if(documentsArray[i].status === 'pending'){
+                for (var i = 0; i < documentsArray.length; i++) {
+                    if (documentsArray[i].status === 'pending') {
                         return 'p';
                     }
                 }
-                for(var i = 0; i < documentsArray.length; i++){
-                    if(documentsArray[i].status === 'declined'){
+                for (var i = 0; i < documentsArray.length; i++) {
+                    if (documentsArray[i].status === 'declined') {
                         return 'd';
                     }
                 }
             }
             return 'n';
         };
-        
+
 
     }
 })();
